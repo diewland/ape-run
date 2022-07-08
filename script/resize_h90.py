34ee5429077b9a7999c7708946180b77e9dbae98
diewland <diewland@gmail.com>
2022-07-09 01:05:37 +0700

    crop h90 done

---
+++ script/resize_h90.py
@@ -0,0 +1,19 @@
+from PIL import Image
+
+def resize_h90(im):
+    # trim
+    im2 = im.crop(im.getbbox())
+    # resize height 90
+    w, h = im2.size
+    nh = 90
+    nw = int((nh * w)/h)
+    return im2.resize((nw, nh))
+
+# 90-3998
+for i in range(90, 3999):
+    src =  "og/%d.png" % i
+    dst =  "h90/%d.png" % i
+    print(src, '->', dst)
+    im = Image.open(src)
+    im2 = resize_h90(im)
+    im2.save(dst)
